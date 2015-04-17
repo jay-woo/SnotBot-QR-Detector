092e811224dd243a7a6a795547d2b6037cf4dff5
Olin Robotics <olin@olin-CF-31SPLAX1M.(none)>
2015-04-17 12:42:49 -0400

    Changed around a bunch of files, and added Python script to control the Pixhawk

--- scripts/qr_listener.py
+++ scripts/qr_listener.py
@@ -1,18 +1,65 @@
 #!/usr/bin/env python
 import rospy
 import roslib
+import time
 roslib.load_manifest('roscopter')
-import roscopter.geometry_msgs
 from geometry_msgs.msg import Quaternion
+import roscopter.msg
 
-def callback(data):
-	data = [data.x, data.y, data.z, data.w]
-	print data
+from std_srvs.srv import *
+from std_msgs.msg import String, Header, Int32
+from geometry_msgs.msg import Twist, Pose
+from std_msgs.msg import Empty
+from sensor_msgs.msg import Joy, Imu, NavSatStatus, NavSatFix
+from datetime import datetime
+from roscopter.srv import APMCommand
+from roscopter.msg import VFR_HUD, State
 
-def listener():
-	rospy.init_node('qr_listener', anonymous=False)
-	rospy.Subscriber('qr_data', Quaternion, callback)
-	rospy.spin()
+class QR_Listener:
+	def __init__(self):
+		rospy.init_node('qr_listener', anonymous=False)
+
+		self.twist = [0, 0, 0, 0, 1500, 1500, 1500, 1500]
+		self.x = 1500.
+		self.y = 1500.
+		self.z = 1000.
+		self.yaw = 1500.
+		self.alt = 0.
+		self.armed = False
+		self.qr_found = False
+
+		self.qr_data = rospy.Subscriber('vision', Quaternion, self.callback)
+		self.pub_rc = rospy.Publisher('/send_rc', roscopter.msg.RC)
+		self.sub_state = rospy.Subscriber('/state', State, self.check_armed)
+		self.command_serv = rospy.ServiceProxy('command', APMCommand)
+
+		r = rospy.Rate(20)
+		while not rospy.is_shutdown():
+			self.search()
+			r.sleep()
+
+	def callback(self, data):
+		self.data = [data.x, data.y, data.z, data.w]
+
+	def check_armed(self, data):
+		self.armed = data.armed
+
+	def parse_action(self, data):
+		self.airspeed = data.airspeed
+		self.groundspeed = data.groundspeed
+		self.heading = data.heading
+		self.throttle = data.throttle
+		self.alt = data.alt
+		self.plt_alt.append(self.alt)
+		self.climb = data.climb
+
+	def search(self):
+ 		self.command_serv(3)
+ 		if not self.qr_found:
+			(self.twist[0], self.twist[1], self.twist[2], self.twist[3]) = (1500, 1500, 1550, 1500)
+		else:
+			(self.twist[0], self.twist[1], self.twist[2], self.twist[3]) = (1500, 1500, 1500, 1500)
+		self.pub_rc.publish(self.twist)
 
 if __name__ == '__main__':
-	listener()	
+	var = QR_Listener()
