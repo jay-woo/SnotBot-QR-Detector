43ad15f70f4077b3d0e27a99a9678238de9b3517
Olin Robotics <olin@olin-CF-31SPLAX1M.(none)>
2015-04-27 13:48:56 -0400

    Merged joysnot code with vision code

--- scripts/qr_listener.py
+++ scripts/qr_listener.py
@@ -13,62 +13,82 @@ from std_msgs.msg import Empty
 from sensor_msgs.msg import Joy, Imu, NavSatStatus, NavSatFix
 from datetime import datetime
 from roscopter.srv import APMCommand
-from roscopter.msg import VFR_HUD, State
+from roscopter.msg import State
 
-class QR_Listener:
-	def __init__(self):
-		rospy.init_node('qr_listener', anonymous=False)
+class MCN():
 
-		self.twist = [0, 0, 0, 0, 1500, 1500, 1500, 1500]
-		self.x = 1500.
-		self.y = 1500.
-		self.z = 1000.
-		self.yaw = 1500.
-		self.alt = 0.
-		self.armed = False
-		self.qr_found = False
-		self.data = [0., 0., 0., 0.]
+    def __init__(self):
+        rospy.init_node('listener', anonymous=False)
+        self.axes = []
+        self.buttons = []
+        self.twist = [0, 0, 0, 0, 1500, 1500, 1500, 1500]
+        self.x = 1500.0 #Side Tilt
+        self.y = 1500.0 #Front Tilt
+        self.z = 1000.0 #Throttle
+        self.yaw = 1500 #Spin
+        self.armed = False
+        self.qr_found = False
+        self.data = [0., 0., 0., 0.]
 
-		self.qr_data = rospy.Subscriber('vision', Quaternion, self.callback)
-		self.pub_rc = rospy.Publisher('/send_rc', roscopter.msg.RC)
-		self.sub_state = rospy.Subscriber('/state', State, self.check_armed)
-		self.command_serv = rospy.ServiceProxy('command', APMCommand)
+        self.qr_data = rospy.Subscriber('vision', Quaternion, self.vision_callback)
+        self.pub_rc = rospy.Publisher('/send_rc', roscopter.msg.RC)
+        self.sub_joy = rospy.Subscriber("/joy", Joy, self.joy_callback)
+        self.sub_state = rospy.Subscriber("/state", State, self.check_state)
+        self.command_serv = rospy.ServiceProxy('command', APMCommand)
 
-		r = rospy.Rate(20)
-		while not rospy.is_shutdown():
-			self.search()
-			r.sleep()
+        r = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            self.fly()
+            r.sleep()
 
-	def callback(self, data):
-		old_data = self.data
-		self.data = [data.x, data.y, data.z, data.w]
-		self.qr_found = old_data != self.data
+    def check_state(self, data):
+        self.armed = data.armed
 
-	def check_armed(self, data):
-		self.armed = data.armed
+    def vision_callback(self, data):
+        old_data = self.data
+        self.data = [data.x, data.y, data.z, data.w]
+        self.qr_found = old_data != self.data
 
-	def parse_action(self, data):
-		self.airspeed = data.airspeed
-		self.groundspeed = data.groundspeed
-		self.heading = data.heading
-		self.throttle = data.throttle
-		self.alt = data.alt
-		self.plt_alt.append(self.alt)
-		self.climb = data.climb
+    def joy_callback(self, data):
+        self.axes = data.axes
+        self.buttons = data.buttons
+        self.x = 1500-self.axes[0]*300 #Scales 1200-1800
+        self.y = 1500-self.axes[1]*300 #Scales 1200-1800
+        self.z = 2000+(self.axes[3])*1000 #Scales 1000-2000
+        self.yaw = 1500-self.axes[2]*300 #Scales 1200-1800
 
-	def search(self):
-		if not self.armed:
-			time.sleep(10)
+    def fly(self):
+        if self.buttons:
+            if self.buttons[2]:
+                self.command_serv(roscopter.srv.APMCommandRequest.CMD_ARM)
+                print 'Arm Quad'
+            if self.buttons[3]:
+                self.command_serv(4)
+                print 'Disarm Quad'
+            if self.buttons[8]:
+                self.command_serv(7)
+                print 'Stabilize'
+            if self.buttons[10]:
+                self.command_serv(11)
+                print 'Land'
+            if self.buttons[11]:
+            	self.search()
+            	print 'Find Fiducial'
+        
+        if self.armed:
+            (self.twist[0], self.twist[1], self.twist[2], self.twist[3]) = (int(self.x), int(self.y), int(self.z), int(self.yaw))
+            self.pub_rc.publish(self.twist)
 
- 			self.command_serv(3)
- 			self.armed = True
-
- 		if self.armed:
-	 		if not self.qr_found:
-				(self.twist[0], self.twist[1], self.twist[2], self.twist[3]) = (1700, 1500, 1500, 1500)
-			else:
-				(self.twist[0], self.twist[1], self.twist[2], self.twist[3]) = (1500, 1500, 1500, 1500)
-			self.pub_rc.publish(self.twist)
+    def search(self):
+        while ~self.qr_found:
+            self.x = 1800
+            (self.twist[0], self.twist[1], self.twist[2], self.twist[3]) = (int(self.x), int(self.y), int(self.z), int(self.yaw))
 
 if __name__ == '__main__':
-	var = QR_Listener()
+    print 'process started at ' + str(datetime.now())
+    try:
+        var = MCN()
+        rospy.spin()
+    except rospy.ROSInterruptException: 
+        pass
+    #TODO shutdown when ROS exited
